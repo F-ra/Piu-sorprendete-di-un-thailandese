026d842fb47636e3edcafde3541b69f2fc5324e8
James Brot <134499028+F-ra@users.noreply.github.com>
2023-06-01 10:20:18 +0000

    Ho rubato l'acqua dall'afghanistan

--- prova.py
+++ prova.py
@@ -7,3 +7,22 @@ b=3
 print('a =',a)
 print('b =',b)
 print('a+b =',a+b)
+
+xc = input("Bel Culo: ")
+
+bum = input("nice ass: ")
+
+print("", )
+
+def stringconcat(str1, str2):
+    gulag = []
+    a = 0
+    for v in range(len(str1) + len(str2)):
+        gulag[a] = str1[a]
+        a = a + 1
+        if a == (len(str1) - 1):
+            a = 0
+            break
+    print(gulag)
+
+stringconcat(xc, bum)
